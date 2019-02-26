c39ac3d47cf7968c7fd73e60c272eae96a675346
flashkosta <47536236+flashkosta@users.noreply.github.com>
2019-02-27 01:09:15 +0300

    upd

---
+++ www/tests/test1.js
@@ -0,0 +1,6 @@
+
+// TASK 1
+var task_name1 = "Бабцик - Лидер, Оберхоф, 1999";
+var task_point1 = 3;
+var task_fen1 = "r2qr1k1/1Qp2ppp/p2p4/4b3/8/1PN5/P4PPP/2R1R1K1 b - - 0 1";
+var task_steps1 = ["Bxc3", "Rxe8", "Qxe8"];
